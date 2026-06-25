77fdd777a9319228b2453f57a7832038e4d76ab7
LucaMacheda <lucamacheda200906@gmail.com>
2026-06-25 12:27:00 +0900

    Update LoginFrame.java

--- LoginFrame.java
+++ LoginFrame.java
@@ -1,1 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
 
+public class LoginFrame extends JFrame {
+    private JTextField txtUsername;
+    private JPasswordField txtPassword;
+    private JButton btnLogin;
+    private PlayerService playerService;
+
+    public LoginFrame() {
+        playerService = new PlayerService();
+
+        setTitle("Login");
+        setSize(350, 220);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel(new GridLayout(3, 2, 10, 10));
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        panel.add(new JLabel("Username:"));
+        txtUsername = new JTextField();
+        panel.add(txtUsername);
+
+        panel.add(new JLabel("Password:"));
+        txtPassword = new JPasswordField();
+        panel.add(txtPassword);
+
+        panel.add(new JLabel(""));
+        btnLogin = new JButton("Login");
+        panel.add(btnLogin);
+
+        add(panel);
+
+        btnLogin.addActionListener(e -> {
+            String username = txtUsername.getText();
+            String password = new String(txtPassword.getPassword());
+
+            Player player = playerService.login(username, password);
+
+            if (player != null) {
+                JOptionPane.showMessageDialog(this, "Login successful!");
+                MainMenuFrame menuFrame = new MainMenuFrame(player);
+                menuFrame.setVisible(true);
+                this.dispose();
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid username or password!");
+            }
+        });
+    }
+}
